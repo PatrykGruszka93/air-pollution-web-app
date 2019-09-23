dd95ff86e6e785fec0c7460dd97346eed4d76f0b
PatrykGruszka93 <p.gruszka93@wp.pl>
2019-09-23 21:35:30 +0200

    First commit

---
+++ server/air-pollution-web-app/src/main/java/com/gruszka/airpollutionwebapp/AirPollutionWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.gruszka.airpollutionwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirPollutionWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirPollutionWebAppApplication.class, args);
+	}
+
+}
